19ec4f28e18c618179fe0943df15745a1a1d196d
Diego Miranda Olavarría <dhmiraola@gmail.com>
2023-10-26 17:23:37 -0300

    Add files via upload

---
+++ Unidad 3/trabajo3.R
@@ -0,0 +1,20 @@
+# Tarea acumulativa 3, Calcule las probabilidades. 
+# Ademas de los calculos distinga los diferentes valores y escriba los planteamientos en notacion estadistica.
+
+
+# 1) Una fabrica de productos electronicos tiene un 10% fabricados defectuosos. Sete toma una muestra aleatoria
+# de 20 productos. 
+# a) calcule la probabilidad de que 6 de los 20 productos sean defectuosos
+# b) la probabilidad que 3 o menos productos sean defectuosos
+
+# 2) un centro de llamadas recibe en promedio 20 llamadas por minuto.
+# a) Calcule la probabilidad de que reciba 15 llamadas por minuto 
+# b) la probabilidad de que reciba menos de 15 o menos llamadas
+
+
+# 3) las notas del examen en una estadistica siguen una distribución normal con una media 70 
+# y una desviación estándarde 10
+# a) calcular la probabilidad de que un estudiante obtenga una calificación sea un 5
+# b) La provabilidad que sea mayor o igual a 6
+
+
